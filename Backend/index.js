6c88023c7077f235b902f4cb75b230abf9f33b9d
swayamjain1240 <swayamjain1240@gmail.com>
2026-09-03 21:55:43 +0530

    modify index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,17 +16,26 @@ import validationRoutes from "./routes/validation.routes.js";
 import dashboardRoutes from "./routes/dashboard.routes.js";
 
 const app = express()
-
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 const PORT = process.env.PORT;
 app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 app.use(cookieParser());
 
 app.get("/api/health", (req,res)=>{
-    res.status(200).json({success: true,message: "Fluxor backend is running",});
+    return res.status(200).json({success: true,message: "Fluxor backend is running",});
 });
 
 app.use("/api/auth", authRouter);
-app.use("/api/auth", authRoutes);
 
 app.use("/api/datasets", datasetRoutes);
 
@@ -39,6 +48,13 @@ app.use("/api/reports", reportRoutes);
 app.use("/api/validations", validationRoutes);
 
 app.use("/api/dashboard", dashboardRoutes);
+
+app.use((req, res) => {
+    return res.status(404).json({
+        success: false,
+        message: "Route not found",
+    });
+});
 
 
 app.listen(PORT,()=>{
